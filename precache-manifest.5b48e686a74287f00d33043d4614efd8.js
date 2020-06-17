869c828b6c08b9b84da0f6726c697641912cec81
Mohamed Eleraky <mohamedzaki@hotmail.com>
2020-06-17 23:53:13 +1000

    Updates

---
+++ precache-manifest.5b48e686a74287f00d33043d4614efd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954f566207411836d5700dbe9ef745a8",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "3f479572517bd7ebe702",
+    "url": "/employee-directory/static/css/main.a4f960a0.chunk.css"
+  },
+  {
+    "revision": "564dda796579cc8e4ab2",
+    "url": "/employee-directory/static/js/2.67f5d01d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/employee-directory/static/js/2.67f5d01d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f479572517bd7ebe702",
+    "url": "/employee-directory/static/js/main.93b8db19.chunk.js"
+  },
+  {
+    "revision": "e735a38aa72cb88a92be",
+    "url": "/employee-directory/static/js/runtime-main.d8c06ce1.js"
+  }
+]);
